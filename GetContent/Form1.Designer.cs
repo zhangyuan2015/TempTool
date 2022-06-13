c1bdc9bb28949f3b4128366b389fd5be3326bf1c
yvan.zhang <yvan.zhang@dji.com>
2022-06-13 21:08:42 +0800

    1

---
+++ GetContent/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GetContent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCookie = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(12, 86);
+            this.txtRes.Multiline = true;
+            this.txtRes.Name = "txtRes";
+            this.txtRes.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtRes.Size = new System.Drawing.Size(776, 352);
+            this.txtRes.TabIndex = 1;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(59, 6);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(143, 21);
+            this.txtKey.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "关键字";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cookie";
+            // 
+            // txtCookie
+            // 
+            this.txtCookie.Location = new System.Drawing.Point(255, 6);
+            this.txtCookie.Multiline = true;
+            this.txtCookie.Name = "txtCookie";
+            this.txtCookie.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCookie.Size = new System.Drawing.Size(533, 74);
+            this.txtCookie.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCookie);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "获取";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCookie;
+    }
+}
+
---
+++ GetContent/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace GetContent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var cookieStr = txtCookie.Text;
+                if (string.IsNullOrEmpty(cookieStr))
+                {
+                    txtRes.Text = "请填入Cookie";
+                    return;
+                }
+
+                CookieCollection cookies = new CookieCollection();
+
+                foreach (var cookieItem in cookieStr.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var cookieItemArr = cookieItem.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (cookieItemArr.Length == 1)
+                        cookies.Add(new Cookie(cookieItemArr[0].Trim(), "", "/", "www.mrobao.com"));
+                    else if (cookieItemArr.Length == 2)
+                        cookies.Add(new Cookie(cookieItemArr[0].Trim(), cookieItemArr[1].Trim(), "/", "www.mrobao.com"));
+                }
+                CookieContainer cookieContainer = new CookieContainer();
+                cookieContainer.Add(cookies);
+
+                WebHeaderCollection webHeaderCollection = new WebHeaderCollection();
+                var responseBody = HttpGet($"http://www.mrobao.com/main.php?m=product&s=admin_sellorder&key={txtKey.Text.Trim()}&buy_catid=&is_invoice=", cookieContainer, webHeaderCollection);
+                txtRes.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                txtRes.Text = ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// GET请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="cookie"></param>
+        /// <returns></returns>
+        public static string HttpGet(string url, CookieContainer cookies, WebHeaderCollection headers)
+        {
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(url);
+            req.CookieContainer = cookies;
+            req.Headers = headers;
+            req.Method = "GET";
+            req.ContentType = "text/html";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36";
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+
+            WebResponse wr = req.GetResponse();
+            Stream respStream = wr.GetResponseStream();
+            StreamReader reader = new StreamReader(respStream, System.Text.Encoding.GetEncoding("utf-8"));
+            string t = reader.ReadToEnd();
+            wr.Close();
+            return t;
+        }
+    }
+}
