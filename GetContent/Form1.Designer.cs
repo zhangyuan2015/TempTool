ce7449d479005eb88bad5c61f2cbc6ee99d4a9c7
zhangyuan <cool.zhangyy@gmail.com>
2022-06-14 01:15:19 +0800

    1

--- GetContent/Form1.Designer.cs
+++ GetContent/Form1.Designer.cs
@@ -34,13 +34,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtCookie = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPageCount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOrderCount = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Location = new System.Drawing.Point(16, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.Size = new System.Drawing.Size(100, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "开始";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,58 +53,106 @@
             // 
             // txtRes
             // 
-            this.txtRes.Location = new System.Drawing.Point(12, 86);
+            this.txtRes.Location = new System.Drawing.Point(16, 108);
+            this.txtRes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtRes.Multiline = true;
             this.txtRes.Name = "txtRes";
             this.txtRes.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtRes.Size = new System.Drawing.Size(776, 352);
+            this.txtRes.Size = new System.Drawing.Size(1033, 439);
             this.txtRes.TabIndex = 1;
             // 
             // txtKey
             // 
-            this.txtKey.Location = new System.Drawing.Point(59, 6);
+            this.txtKey.Location = new System.Drawing.Point(79, 8);
+            this.txtKey.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtKey.Name = "txtKey";
-            this.txtKey.Size = new System.Drawing.Size(143, 21);
+            this.txtKey.Size = new System.Drawing.Size(189, 25);
             this.txtKey.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "关键字";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(208, 9);
+            this.label2.Location = new System.Drawing.Point(277, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.Size = new System.Drawing.Size(55, 15);
             this.label2.TabIndex = 5;
             this.label2.Text = "Cookie";
             // 
             // txtCookie
             // 
-            this.txtCookie.Location = new System.Drawing.Point(255, 6);
+            this.txtCookie.Location = new System.Drawing.Point(340, 8);
+            this.txtCookie.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCookie.Multiline = true;
             this.txtCookie.Name = "txtCookie";
             this.txtCookie.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtCookie.Size = new System.Drawing.Size(533, 74);
+            this.txtCookie.Size = new System.Drawing.Size(709, 92);
             this.txtCookie.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 48);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "页数";
+            // 
+            // txtPageCount
+            // 
+            this.txtPageCount.Location = new System.Drawing.Point(169, 41);
+            this.txtPageCount.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPageCount.Name = "txtPageCount";
+            this.txtPageCount.ReadOnly = true;
+            this.txtPageCount.Size = new System.Drawing.Size(163, 25);
+            this.txtPageCount.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 74);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "单数";
+            // 
+            // txtOrderCount
+            // 
+            this.txtOrderCount.Location = new System.Drawing.Point(169, 71);
+            this.txtOrderCount.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOrderCount.Name = "txtOrderCount";
+            this.txtOrderCount.ReadOnly = true;
+            this.txtOrderCount.Size = new System.Drawing.Size(163, 25);
+            this.txtOrderCount.TabIndex = 9;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.txtOrderCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPageCount);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCookie);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtKey);
             this.Controls.Add(this.txtRes);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "获取";
             this.ResumeLayout(false);
@@ -115,6 +168,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtCookie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPageCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOrderCount;
     }
 }
 
--- GetContent/Form1.cs
+++ GetContent/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Windows.Forms;
@@ -43,9 +44,51 @@ namespace GetContent
 
                 var doc = new HtmlDocument();
                 doc.LoadHtml(responseBody);
-                HtmlNode headNode = doc.DocumentNode.SelectSingleNode("/html/body/div[6]/div[2]/div[2]/div/table/tfoot/tr/td/div[2]");
-                string Title = headNode.InnerText;
-                txtRes.Text = Title;
+                var orderNode = doc.DocumentNode.SelectSingleNode("//table[@class='table-list-style order']");
+                var orderTrNodes = orderNode.SelectNodes("//tbody/tr");
+
+                List<订单> 订单集合 = new List<订单>();
+
+                int q = 4;
+                订单 订单 = null;
+                for (int i = 0; i < orderTrNodes.Count; i++)
+                {
+
+                    if (i % q == 0)
+                    {
+                        订单 = new 订单();
+                        //var a = orderTrNodes[i].InnerText;
+                    }
+                    else if (i % q == 1)
+                    {
+                        var 订单编号 = orderTrNodes[i].SelectSingleNode("//th/span[1]/span").InnerText.Trim();
+                        var 下单时间 = orderTrNodes[i].SelectSingleNode("//th/span[2]/span").InnerText.Trim();
+
+                        订单.订单编号 = 订单编号;
+                        订单.下单时间 = 下单时间;
+                    }
+                    else if (i % q == 2)
+                    {
+                        var a = orderTrNodes[i].InnerText;
+                    }
+                    else if (i % q == 3)
+                    {
+                        if (orderTrNodes[i].InnerText.Contains("发票号码"))
+                        {
+                            var a = orderTrNodes[i].SelectSingleNode("//td");
+                            var 发票信息 = orderTrNodes[i].SelectSingleNode("//td/p").InnerText.Trim();
+                            订单.发票信息 = new 发票信息 { 发票号码 = 发票信息 };
+                        }
+
+                        订单集合.Add(订单);
+                    }
+                }
+
+                var totalPage = doc.DocumentNode.SelectSingleNode("//div[@class='pagination']/a[10]").InnerText.Trim('.');
+                txtPageCount.Text = $"1 / {totalPage}";
+
+                //string Title = headNode.InnerText;
+                txtRes.Text = responseBody;
             }
             catch (Exception ex)
             {
@@ -76,5 +119,62 @@ namespace GetContent
             wr.Close();
             return t;
         }
+    }
+
+    public class 订单
+    {
+        public string 订单编号 { get; set; }
+        public string 下单时间 { get; set; }
+        public string 付款时间 { get; set; }
+        public string 发货时间 { get; set; }
+        public string 物流名称 { get; set; }
+        public string 物流单号 { get; set; }
+
+        public 收货地址 收货地址 { get; set; }
+
+        public 发票信息 发票信息 { get; set; }
+
+        public 买家信息 买家信息 { get; set; }
+
+        public List<订单商品> 订单商品 { get; set; }
+    }
+
+    public class 收货地址
+    {
+        public string 收件人 { get; set; }
+        public List<string> 联系电话 { get; set; }
+        public string 省 { get; set; }
+        public string 市 { get; set; }
+        public string 区 { get; set; }
+        public string 地址 { get; set; }
+    }
+
+    public class 发票信息
+    {
+        public string 公司名称 { get; set; }
+        public string 税号 { get; set; }
+        public string 开户地址 { get; set; }
+        public string 电话 { get; set; }
+        public string 开户银行 { get; set; }
+        public string 帐号 { get; set; }
+
+        public string 发票号码 { get; set; }
+        public string 金额 { get; set; }
+        public string 开票时间 { get; set; }
+    }
+
+    public class 买家信息
+    {
+        public string 用户名 { get; set; }
+        public string 昵称 { get; set; }
+    }
+
+    public class 订单商品
+    {
+        public string 商品 { get; set; }
+        public string 状态 { get; set; }
+        public string 单价_元 { get; set; }
+        public string 数量 { get; set; }
+        public string 商品总价_元 { get; set; }
     }
 }
